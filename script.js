1cd4fd67311126c518a7b1f65f45cbb4f292f34c
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-18 21:47:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class Animal {
         return this._species;
     }
     makeSound(){
-        console.log(this._species + " makes a sound");
+        console.log("The " + this._species + " makes a sound");
     }
 }
 
@@ -16,7 +16,7 @@ class Dog extends Animal {
         super(species);
     }
     bark(){
-        console.log("woof");
+        console.log("The " + this._species + " says woof");
     }
 }
 
@@ -25,7 +25,7 @@ class Cat extends Animal {
         super(species);
     }
     purr(){
-        console.log("purr");
+        console.log("The " + this._species + " says purr");
     }
 }
 
