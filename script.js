2bc4e02d1170198338dd43bb47967a010474adb6
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-18 21:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this._species =species;
+	}
+	get species(){
+		return this._species;
+	}
+	makeSound(){
+		this._species "makes asound";
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(){
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		console.log("purr");
+	}
+}
+const myCat = new Cat("siamese");
+myCat.makeSound();
+myCat.purr();
+
+const myDog = new Dog("golden retriever");
+myDog.makeSound();
+myDog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
